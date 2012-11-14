dd14237dfb88fe718089364236375ea1e881f44e
Haitham Gabr <hgabr@cise.ufl.edu>
2012-11-14 17:47:05 -0500

    target enrichment done

--- target_enrichment/enrichment.rb
+++ target_enrichment/enrichment.rb
@@ -1,16 +1,42 @@
 targets_file = ARGV[0]
 terms_file = ARGV[1]
+output_file = ARGV[2]
 @annotations = {}
 @targets = []
 @terms = []
+@results = []
+@annotation_maps = {}
 
+#targets annotation map of a given term
+def annotations_map(term)
+	@annotation_maps[term] ||= 0.upto(@targets.size-1).map{|i| @annotations[@targets[i]].include?(term) ? 1 : 0}
+end
 
-def success_count(term, top_size)
-	success = 0
-	0.upto(top_size-1) do |i|
-		success = success + 1 if @annotations[@targets[i]].include? term
-	end
-	success
+#number of annotations in the top_size targets for the given term
+def annotations_count(term, top_size)
+	annotations_map(term)[0..top_size-1].reduce(:+)
+end
+
+#ln (n P r)
+def log_perm(n, r)
+	0.upto(r-1).reduce(0){|r, i| r = r + Math.log(n-i)}
+end
+
+#ln (n C r)
+def log_comb(n, r)
+	log_perm(n, r) - log_perm(r, r)
+end
+
+#Hypergeometric P(X = selected_true | universe, universal_true, selection)
+def hypergeometric(universe, draw, partition, success)
+	Math.exp(log_comb(partition, success) + log_comb(universe-partition, draw-success) - log_comb(universe, draw))
+end
+
+#Enrichment of a term using a given top_size
+def enrichment(term, top_size)
+	annotations_count(term, top_size).upto(top_size).map{|success|
+		hypergeometric @targets.size, annotations_count(term, @targets.size), top_size, success
+	}.reduce(:+)
 end
 
 line = ""
@@ -30,4 +56,26 @@ open terms_file do |f|
 		@terms |= @annotations[target]
 	end
 end
+
+@terms.each do |term|
+	@results << 1.upto(@targets.size-1).map{|top_size|
+		{:term => term, :top_size => top_size, :enrichment => enrichment(term, top_size)}
+	}.reduce{|best, current| best[:enrichment] > current[:enrichment] ? current : best}
+end
+
+open output_file, "w" do |f|
+	@results.sort{|a,b| a[:enrichment] <=> b[:enrichment]}.each{|r| f.puts "#{sprintf("%-10s", r[:term])}#{sprintf("%-24s", r[:enrichment])}#{sprintf("%-5s", r[:top_size])}#{sprintf("%-4s", annotations_count(r[:term], @targets.size))}#{annotations_map(r[:term]).join}"}
+end
+
+
+
+
+
+
+
+
+
+
+
+
 
